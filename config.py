3f40c80b11e7205992c76701160f054632179f61
sewaustav <sewabobrov04@gmail.com>
2023-06-30 17:03:26 +0200

    Update config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "5929439884:AAEFGgEfpC7fn1ZCtIMTZ3GgjpdyHPIDKkg"
-#password = "la0ox0k4h8"
-#user = "postgres"
-#host = "80.90.190.84"
-#name = "default_db"
-OpenAITOKEN = "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"
-google_ip_key = "AIzaSyBeAKbZE5K1yZszqGIhs-uSnw5vDgBATGc"
-FOLDER_ID = '1bGol14FPiL4fSjKXeXjIF3qDvf_7vqpD'
-DESTINATION_FOLDER = 'photos'
